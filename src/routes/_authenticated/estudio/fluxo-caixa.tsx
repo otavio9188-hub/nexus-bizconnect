4bfdd291aed12eb89e0b9b14cd9adc07d7d31ba0
otavio9188-hub <otavio.9188@gmail.com>
2026-09-24 23:27:31 -0300

    fix(finance): remove duplicate React import

--- src/routes/_authenticated/estudio/fluxo-caixa.tsx
+++ src/routes/_authenticated/estudio/fluxo-caixa.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import * as React from "react";
 import { createFileRoute } from "@tanstack/react-router";
 import { useQuery } from "@tanstack/react-query";
 import { useServerFn } from "@tanstack/react-start";
