f885895fb599077263172db99fe0e78f7da339af
otavio9188-hub <otavio.9188@gmail.com>
2026-09-24 23:27:25 -0300

    fix(finance): normalize React import in cash flow page

--- src/routes/_authenticated/estudio/fluxo-caixa.tsx
+++ src/routes/_authenticated/estudio/fluxo-caixa.tsx
@@ -1,3 +1,5 @@
+import * as React from "react";
+import * as React from "react";
 import { createFileRoute } from "@tanstack/react-router";
 import { useQuery } from "@tanstack/react-query";
 import { useServerFn } from "@tanstack/react-start";
